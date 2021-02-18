8b7590ee1d742aa9dd9a54c9653f7c37ea3fcd9d
martinckf <martin.ckf@outlook.com>
2021-02-18 14:29:18 +0800

    Course Completed

--- get-quote.py
+++ get-quote.py
@@ -5,9 +5,19 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  last = 16
+  rnd = random.randint(0,last)
+  print("Print from quotes.txt")
+  print(quotes[rnd], end='')
+  print(" ")
+
+  f = open("quotes_output.txt","a")
+  f.write(quotes[rnd])
+  f.close
+  f = open("quotes_output.txt","r")
+  print("Print from quotes_output.txt")
+  print(f.read( ))
+  f.close
 
 if __name__== "__main__":
   primary()
